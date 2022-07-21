e1409ff93fbf14f2aab4a14d52ebef3d3b071eb6
Avre Barra <avreghly@gmail.com>
2022-07-21 16:00:18 +0700

    add basic routes

--- go.mod
+++ go.mod
@@ -3,9 +3,16 @@ module github.com/avrebarra/kubebe
 go 1.18
 
 require (
+	github.com/avrebarra/minivalidator v1.0.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/rs/zerolog v1.27.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/avrebarra/minivalidator"
+	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
@@ -11,6 +18,8 @@ import (
 
 var (
 	appname       = "kubebe"
+	port          = 2323
+	starttime     = time.Now()
 	prettylogging = true
 )
 
@@ -20,12 +29,114 @@ func main() {
 	if prettylogging {
 		logsink = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 	}
-	logsink = logsink.With().Str("app", appname).Timestamp().Logger()
+	logsink = logsink.With().Str("app", appname).Int("port", port).Timestamp().Logger()
 
 	stdlog.SetFlags(0)
 	stdlog.SetOutput(logsink.With().Str("level", "debug").Logger())
 	log.Logger = logsink
 
-	// log something
-	stdlog.Println("initializing server...")
+	// prepare basic server
+	stdlog.Println("setting up server...")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HandleIndex()).Methods("GET")
+	r.HandleFunc("/diceroll", HandleDiceRoll()).Methods("POST")
+
+	// prepare basic server
+	stdlog.Printf("using port http://localhost:%d to start server... ", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+	if err != nil {
+		err = fmt.Errorf("cannot start server: %w", err)
+		panic(err)
+	}
+}
+
+// ***
+
+func HandleIndex() http.HandlerFunc {
+	type ResponseData struct {
+		ID             string    `json:"id"`
+		State          string    `json:"state"`
+		StartTimestamp time.Time `json:"start_time"`
+		Uptime         string    `json:"uptime"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		respdata := Response{
+			Code: "ok",
+			Data: ResponseData{
+				ID:             appname,
+				State:          "healthy",
+				StartTimestamp: starttime,
+				Uptime:         time.Since(starttime).Round(time.Second).String(),
+			},
+		}
+		respond(w, http.StatusOK, respdata)
+	}
+}
+
+func HandleDiceRoll() http.HandlerFunc {
+	type RequestData struct {
+		DiceNumber int `json:"dice_number" validate:"required"`
+	}
+	type ResponseData struct {
+		Total    int   `json:"total"`
+		Sequence []int `json:"sequence"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// handle input
+		reqdata := RequestData{}
+		if err := json.NewDecoder(r.Body).Decode(&reqdata); err != nil {
+			respdata := Response{Code: "bad", Message: err.Error()}
+			respond(w, http.StatusInternalServerError, respdata)
+			return
+		}
+		if err := minivalidator.Validate(reqdata); err != nil {
+			respdata := Response{Code: "bad", Message: err.Error()}
+			respond(w, http.StatusInternalServerError, respdata)
+			return
+		}
+
+		// process request
+		sequence := []int{}
+		total := 0
+		for i := 0; i < reqdata.DiceNumber; i++ {
+			num := rand.Intn(6-1) + 1
+			total += num
+			sequence = append(sequence, num)
+		}
+
+		// prepare output
+		respdata := Response{
+			Code: "ok",
+			Data: ResponseData{
+				Sequence: sequence,
+				Total:    total,
+			},
+		}
+		respond(w, http.StatusOK, respdata)
+	}
+}
+
+// ***
+
+type Response struct {
+	Code    string `json:"code"`
+	Message string `json:"message,omitempty"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func respond(writer http.ResponseWriter, status int, data any) {
+	respout, err := json.Marshal(data)
+	if err != nil {
+		err = fmt.Errorf("cannot marshall output: %w", err)
+		log.Error().Err(err).Msg("marshall failure")
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(respout)
+
+	log.Info().Msg("request finished")
 }
